f49fcf576bb8b2cf641b7ed785e6f148b580578e
Anzela24 <56473376+Anzela24@users.noreply.github.com>
2019-10-26 12:59:11 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,11 @@
+#ini variabel
+
+namalengkap = "Anzela Hasan Thalia Putri"
+namapanggilan = "zela"
+npm = "311910663"
+tempatlahir = "Indramayu"
+umur = 19
+telf = "08980283356"
+alamat = "indramayu kandanghaur"
+
+print("assalamualaikum,\n let me introduce my self. my name is",namalengkap,",but you can call me",namapanggilan,",my npm is",npm,", I was born in",tempatlahir,"and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,",so dont forget to call me before with the number",telf,"thank you")
